19f9b021c4c8098ba23793a86e43ca7e4081d724
Idriz Mehmedovic <idriz.96@hotmail.com>
2022-04-17 15:13:28 +0200

    change firebase environments

--- src/Shared/Constants/index.js
+++ src/Shared/Constants/index.js
@@ -1,6 +1,6 @@
 export const EMAIL_PATTERN = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 export const PHONE_NUMBER_PATTERN = /^\+?\d+$/;
-export const FIREBASE_API_KEY = 'AIzaSyCx5VswJGEb2IQ7iTxjSI6UFiVfJWRFafM';
+export const FIREBASE_API_KEY = 'AIzaSyBqd8erqcC0Oe9fJ-KS2SNRB4eU1zRaBeo';
 export const FIREBASE_SIGNIN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API_KEY}`; //{"email":"[user@example.com]","password":"[PASSWORD]","returnSecureToken":true}
 export const FIREBASE_SIGNUP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FIREBASE_API_KEY}`; //{"email":"[user@example.com]","password":"[PASSWORD]","returnSecureToken":true}
-export const FIREBASE_DATABASE = `https://phone-store-fe2c3-default-rtdb.firebaseio.com/`;
+export const FIREBASE_DATABASE = `https://phone-store-im-default-rtdb.firebaseio.com/`;
